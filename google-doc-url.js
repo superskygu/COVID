2eaa9dd58b772d653466369e9b3ee479510a0de6
superskygu <73080210+superskygu@users.noreply.github.com>
2020-12-03 06:00:37 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vT_uVszudwPuMx4NMLnYbrT_BboXHLvZJ9n_7ZBCzxljFf2qa683h1-zPi_0y59GdgJS2MnfHH7O-32/pubhtml';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1yJxBEXW4Lskf1ga2pK0hXbT4Gg0dv_sQHsULUKbCNck/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
+var googleApiKey = '';
